8644a0746a13aa819c5747aaa731dc28a250db21
FabioCoutinho <fabiofarias549@gmail.com>
2025-02-21 17:27:12 -0300

    Update script.js

--- fetch/form/script.js
+++ fetch/form/script.js
@@ -1,17 +1,16 @@
-
-const form = document.querySelector('form');
-form.addEventListener('submit', async (e) =>{
-    e.preventDefault();
-    const formData = new FormData(form);
-    const data = Object.fromEntries(formData);
-    console.log(data);
-    fetch('https://reqres.in/api/users', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    }).then(res => res.json())
-    .then(data => console.log(data));
-});  
-
+const form = document.querySelector("form");
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData);
+  console.log(data);
+  fetch("https://reqres.in/api/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+});
